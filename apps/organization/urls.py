a3bf93f001670fa979a7bc3cc9fe82c9043cb5e6
luminous <zhi_dong@outlook.com>
2017-03-05 17:02:27 +0800

    initial commit

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -4,10 +4,10 @@ __date__ = '2017/3/5 10:17'
 
 from django.conf.urls import url, include
 
-from .views import OrgView
+from .views import OrgView, AddUserAskView
 
 urlpatterns = [
     # 课程机构列表页
     url(r'^list/$', OrgView.as_view(), name="org_list"),
-    url(r'^add_ask/$')
+    url(r'^add_ask/$', AddUserAskView.as_view(), name="add_ask")
 ]
--- apps/organization/views.py
+++ apps/organization/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render
 from django.views.generic import View
 from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
+from django.http import HttpResponse
 
 from .models import CourseOrg, CityDict
 from .forms import UserAskForm
@@ -61,7 +62,13 @@ class OrgView(View):
 
 
 class AddUserAskView(View):
+    """
+    用户添加咨询
+    """
     def post(self, request):
         userask_form = UserAskForm(request.POST)
-        if userask_form.valid():
+        if userask_form.is_valid():
             user_ask = userask_form.save(commit=True)
+            return HttpResponse("{'status':'success'}", content_type='application/json')
+        else:
+            return HttpResponse("{'status':'fail', 'msg':'添加出错'}", content_type='application/json')
